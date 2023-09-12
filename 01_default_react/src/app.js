a07e5b9dc70914c09b317f6096dcbce5eb414424
KevalKThumar <thumarkeval12@gmail.com>
2023-09-12 13:54:40 +0530

    first commit

---
+++ 01_default_react/src/app.js
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <>
+      <h1>Chai aur code | Keval Thumar</h1>
+      <h3>
+        1) how to setup React with 2 different Method first is basic npx
+        create-react-app project_name second is npm create vite@letest</h3>
+        <h3> 2) what
+        is vite and why we use it?? Vite is a bundlur for react app its provide
+        au what we require more for devlopment and light weight app. Vite is an
+        open-source tool that helps you build web apps faster. It's built by the
+        same people behind Vue, React, Svelte. Vite is an open source project
+        that aims to provide the best DX and performance by leveraging native ES
+        modules. It's also blazing fast.
+      </h3>
+    </>
+  );
+}
+
+export default App;
