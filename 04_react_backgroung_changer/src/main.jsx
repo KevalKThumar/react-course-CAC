79ec0c183a2047c8cecbb278f2906d4fdc79f966
KevalKThumar <thumarkeval12@gmail.com>
2023-09-14 13:26:41 +0530

    changes in BGchanger

--- 04_react_backgroung_changer/src/main.jsx
+++ 04_react_backgroung_changer/src/main.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Example from "./Component/Listview";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { SignInOne } from './Component/SignupPage.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* <App /> */}
+    <SignInOne/>
+    {/* <Example/> */}
+  </React.StrictMode>
+);
